52b58c9c12840ff709e5a18050cf87899e27db1d
Tito <107757681+pappitito@users.noreply.github.com>
2023-01-26 22:41:49 +0100

    Update order.js

--- src/pages/order.js
+++ src/pages/order.js
@@ -1,9 +1,17 @@
 import Header from '../components/header'
 function Order(){
     return(
-       <div>
+        <div>
             <Header/>
-             <h2>About us page</h2>
+             <h2 className='motdLabel'>Orders page</h2>
+             <div className='info'>
+          
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+           doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+            veritatis  sunt explicabo. Nemo enim
+             sit amet, consectetur, 
+               adipisci velit, sed quia non numquam</p>
+        </div>
        </div>
     )
 }
