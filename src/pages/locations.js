75f5b8071d500a8369a8fd9799f6aecdf9f6ff8e
Tito <107757681+pappitito@users.noreply.github.com>
2023-01-26 22:41:19 +0100

    Update locations.js

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -3,7 +3,15 @@ function Locations(){
     return(
        <div>
             <Header/>
-             <h2>About us page</h2>
+             <h2 className='motdLabel'>Locations Page</h2>
+             <div className='info'>
+          
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+           doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+            veritatis  sunt explicabo. Nemo enim
+             sit amet, consectetur, 
+               adipisci velit, sed quia non numquam</p>
+        </div>
        </div>
     )
 }
